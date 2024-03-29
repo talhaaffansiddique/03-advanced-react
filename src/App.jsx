83ad981134150dfa48f530380d8266dcf7617387
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-03-29 10:35:50 +0500

    83. UseState Array Example - Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Starter from "./tutorial/01-useState/starter/01-error-example";
-import Final from "./tutorial/01-useState/final/01-error-example";
+import Starter from "./tutorial/01-useState/starter/03-useState-array";
+import Final from "./tutorial/01-useState/final/03-useState-array";
 
 function App() {
   return (
     <div className="container">
-      <h2>Advanced React</h2>
+      <Starter />
+      {/* <Final /> */}
     </div>
   );
 }
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,21 @@
+import { data } from "../../../data";
+import React, { useState } from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default UseStateArray;
