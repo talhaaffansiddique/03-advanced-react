b646f70964712f57b6fd685e1d89dbf7a2488742
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-04-03 09:50:01 +0500

    90 - useState functional update form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/01-useState/starter/04-useState-object";
+import Starter from "./tutorial/01-useState/starter/05-useState-gotcha";
 import Final from "./tutorial/01-useState/final/03-useState-array";
 
 function App() {
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 
 const UseStateObject = () => {
-  const [name, setName] = useState("peter");
-  const [age, setAge] = useState(24);
-  const [hobby, setHobby] = useState("read books");
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 23,
+    hobby: "read books",
+  });
 
-  const displayPerson = () => {
-    setName("John");
-    setAge("15");
-    setHobby(" scream ");
-  };
+  // const [name, setName] = useState("peter");
+  // const [age, setAge] = useState(24);
+  // const [hobby, setHobby] = useState("read books");
+
+  // const displayPerson = () => {
+  //   setName("John");
+  //   setAge("15");
+  //   setHobby(" scream ");
+  // };
+
+  setPerson({ name: "john", age: 15, hobby: "read book" });
 
   return (
     <>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h4>enjoys: {hobby}</h4>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>enjoys: {person.hobby}</h4>
       <button className="btn" onClick={displayPerson}>
         Show John
       </button>
